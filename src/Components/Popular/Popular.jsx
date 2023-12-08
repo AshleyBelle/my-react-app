c80b8076eef84b962fb4d9b698506dd1c5ced1e3
HP <ashleybelle780@gmail.com>
2023-12-08 17:50:28 +0800

    550pm

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import './Popular.css'
+import Item from '../Item/Item'
 //database based on assets\data named data_product
 
 const Popular = () => {
   return (
     <div className='popular'>
-        <h1>Popular</h1>
+        <h1>Popular For Women</h1>
+        <hr />
+        <div className="popular-item">
+            
+
+        </div>
 
     </div>
   )
 }
 
 export default Popular
+
+//{data_product.map((item,i)=>{
+//    return <Item /*ari ang contents na naa sa backend*/ />
+//})}
