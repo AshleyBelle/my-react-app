9a6faef8b2dd086a7a59b25bfe3461e0f3c12ded
HP <ashleybelle780@gmail.com>
2023-12-08 16:52:43 +0800

    huuuhuuuu

---
+++ src/Components/Popular/Popular.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Popular.css'
+//database based on assets\data named data_product
+
+const Popular = () => {
+  return (
+    <div className='popular'>
+        <h1>Popular</h1>
+
+    </div>
+  )
+}
+
+export default Popular
