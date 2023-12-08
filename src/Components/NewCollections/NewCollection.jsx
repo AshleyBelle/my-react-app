e9f740fedf903e85d149304b8c60cc6dd5600ed2
HP <ashleybelle780@gmail.com>
2023-12-08 20:38:05 +0800

    haaaaaaaaaaa

---
+++ src/Components/NewCollections/NewCollection.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './NewCollection.css'
+
+const NewCollection = () => {
+  return (
+    <div className='new-collections'>
+        <h1>New Collections</h1>
+        <hr />
+        <div className="collections">
+            {}
+        </div>
+
+    </div>
+  )
+}
+
+export default NewCollection
