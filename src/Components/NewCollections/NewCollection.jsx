8492af517735a9a777a2211e6eceeedd43bc6142
Lorry Mae <halinalorr19@gmail.com>
2023-12-10 18:28:17 +0800

    uncle brew

--- src/Components/NewCollections/NewCollection.jsx
+++ src/Components/NewCollections/NewCollection.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './NewCollection.css'
+import new_collection from '../Assets/new_collections'
+import Item from '../Item/Item'
 
 const NewCollection = () => {
   return (
@@ -7,7 +9,9 @@ const NewCollection = () => {
         <h1>New Collections</h1>
         <hr />
         <div className="collections">
-            {}
+            {new_collection.map((item, i)=>{
+              return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+            })}
         </div>
 
     </div>
